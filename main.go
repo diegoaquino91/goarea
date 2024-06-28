cf608c3e94eb0f76698f0a590b4d7874be5f920c
Diego Aquino <110973873+diegoaquino91@users.noreply.github.com>
2024-06-27 23:46:14 -0300

    Update main.go
    alterando html

--- main.go
+++ main.go
@@ -1,22 +1,18 @@
-package goarea
+package html
 
-import "math"
+// Titulo obtem o título de uma pagina html
+func Titulo(urls ...string) <-chan string {
+	c := make(chan string)
 
-// Pi é uma proporção númerica definida pela relação entre
-// o perímetro de uma circuferencia e seu diametro
-const Pi = 3.1416
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := io.ReadAll(resp.Body)
 
-// Circ é responsável por calcular a área da circuferencia
-func Circ(raio float64) float64 {
-	return math.Pow(raio, 2) * Pi
-}
+			r, _ := regexp.Compile("<title>(.*?)</title>")
+			c <- r.FindStringSubmatch(string(html))[1]
+		}(url)
+	}
 
-// Rect é responsável por calcular a área de um retangulo
-func Rect(base, altura float64) float64 {
-	return base * altura
-}
-
-// Não é visivel
-func _TrianguloEq(base, altura float64) float64 {
-	return (base * altura) / 2
+	return c
 }
